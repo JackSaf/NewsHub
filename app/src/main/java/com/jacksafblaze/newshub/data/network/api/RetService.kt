e2a2cea31e51398bafa70db42414e887a771188c
JackSaf <jacksafblaze@gmail.com>
2022-09-08 18:28:46 +0300

    renamed network models

--- app/src/main/java/com/jacksafblaze/newshub/data/network/api/RetService.kt
+++ app/src/main/java/com/jacksafblaze/newshub/data/network/api/RetService.kt
@@ -1,6 +1,6 @@
 package com.jacksafblaze.newshub.data.network.api
 
-import com.jacksafblaze.newshub.data.network.model.NewsResult
+import com.jacksafblaze.newshub.data.network.model.NewsNetworkResult
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,5 +9,5 @@ interface RetService {
     @GET("v2/everything")
     suspend fun getSearchedArticles(@Query("apiKey") apiKey: String,
                                     @Query("q") query: String,
-                                    @Query("page")page: Int): Response<NewsResult>
+                                    @Query("page")page: Int): Response<NewsNetworkResult>
 }
