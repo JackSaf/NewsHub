50825da62e01b1de3bcfe82781a972409eb8a5b5
JackSaf <jacksafblaze@gmail.com>
2022-09-10 21:01:49 +0300

    changed columns names + made source field an embedded object

--- app/src/main/java/com/jacksafblaze/newshub/data/database/model/ArticleDbDto.kt
+++ app/src/main/java/com/jacksafblaze/newshub/data/database/model/ArticleDbDto.kt
@@ -1,16 +1,17 @@
 package com.jacksafblaze.newshub.data.database.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.jacksafblaze.newshub.domain.model.Source
 
 @Entity(tableName = "articles")
-data class ArticleDbDto(@PrimaryKey(autoGenerate = true) val uid: Int,
+data class ArticleDbDto(@PrimaryKey(autoGenerate = true) val id: Int,
                         val author: String?,
                         val content: String?,
                         val description: String?,
-                        val publishedAt: String?,
-                        val source: Source?,
+                        @ColumnInfo(name = "published_at") val publishedAt: String?,
+                        @Embedded val source: SourceDbDto?,
                         val title: String?,
                         val url: String?,
-                        val urlToImage: String?)
+                        @ColumnInfo(name = "url_to_image") val urlToImage: String?)
