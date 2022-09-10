64f3c84652fed1d841a0d1a2c094408fda7795ae
JackSaf <jacksafblaze@gmail.com>
2022-09-10 21:15:48 +0300

    deleted entity annotation

--- app/src/main/java/com/jacksafblaze/newshub/data/database/model/SourceDbDto.kt
+++ app/src/main/java/com/jacksafblaze/newshub/data/database/model/SourceDbDto.kt
@@ -1,10 +1,7 @@
 package com.jacksafblaze.newshub.data.database.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "sources")
 class SourceDbDto(
-                @PrimaryKey val id: String,
+                val id: String,
                 val name: String?
 )
