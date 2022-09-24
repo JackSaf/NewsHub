b36f6620e3e4c1d2164e518a78b3d136bc36c4cf
JackSaf <jacksafblaze@gmail.com>
2022-09-24 04:13:21 +0300

    added default query

--- app/src/main/java/com/jacksafblaze/newshub/presentation/SearchNewsViewModel.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/SearchNewsViewModel.kt
@@ -11,6 +11,7 @@ import com.jacksafblaze.newshub.domain.usecase.ViewSearchedNewsUseCase
 import com.jacksafblaze.newshub.presentation.model.UiModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
@@ -18,7 +19,7 @@ class SearchNewsViewModel(
     private val deleteSavedArticleUseCase: DeleteSavedArticleUseCase,
     private val saveArticleUseCase: SaveArticleUseCase,
     private val viewSearchedNewsUseCase: ViewSearchedNewsUseCase): ViewModel() {
-    private val query = MutableStateFlow("")
+    private val query = MutableStateFlow("War")
 
     fun searchForNews(query: String): Flow<PagingData<UiModel>> {
         return viewSearchedNewsUseCase.execute(query).map { pagingData -> pagingData.map { UiModel.NewsItem(it) } }
