91c684647a74860599335703a9619fd6be835811
JackSaf <jacksafblaze@gmail.com>
2022-09-24 04:20:14 +0300

    deleted unused dependencies

--- app/src/main/java/com/jacksafblaze/newshub/presentation/SearchNewsViewModel.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/SearchNewsViewModel.kt
@@ -4,14 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.map
-import com.jacksafblaze.newshub.domain.model.Article
 import com.jacksafblaze.newshub.domain.usecase.DeleteSavedArticleUseCase
 import com.jacksafblaze.newshub.domain.usecase.SaveArticleUseCase
 import com.jacksafblaze.newshub.domain.usecase.ViewSearchedNewsUseCase
 import com.jacksafblaze.newshub.presentation.model.UiModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
@@ -19,7 +17,7 @@ class SearchNewsViewModel(
     private val deleteSavedArticleUseCase: DeleteSavedArticleUseCase,
     private val saveArticleUseCase: SaveArticleUseCase,
     private val viewSearchedNewsUseCase: ViewSearchedNewsUseCase): ViewModel() {
-    private val query = MutableStateFlow("War")
+    private val query = MutableStateFlow("")
 
     fun searchForNews(query: String): Flow<PagingData<UiModel>> {
         return viewSearchedNewsUseCase.execute(query).map { pagingData -> pagingData.map { UiModel.NewsItem(it) } }
