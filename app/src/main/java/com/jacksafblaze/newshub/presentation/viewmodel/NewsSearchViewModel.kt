c3e8bfc2733a0702efb15635f1bc7147050df123
JackSaf <jacksafblaze@gmail.com>
2022-09-28 05:29:41 +0300

    resolved merge issue

--- app/src/main/java/com/jacksafblaze/newshub/presentation/viewmodel/NewsSearchViewModel.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/viewmodel/NewsSearchViewModel.kt
@@ -1,4 +1,4 @@
-package com.jacksafblaze.newshub.presentation
+package com.jacksafblaze.newshub.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,23 +7,29 @@ import androidx.paging.map
 import com.jacksafblaze.newshub.domain.usecase.DeleteSavedArticleUseCase
 import com.jacksafblaze.newshub.domain.usecase.SaveArticleUseCase
 import com.jacksafblaze.newshub.domain.usecase.ViewSearchedNewsUseCase
+import com.jacksafblaze.newshub.domain.usecase.ViewTopHeadlinesUseCase
 import com.jacksafblaze.newshub.presentation.model.UiModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@HiltViewModel
 class NewsSearchViewModel @Inject constructor(
     private val deleteSavedArticleUseCase: DeleteSavedArticleUseCase,
     private val saveArticleUseCase: SaveArticleUseCase,
-    private val viewSearchedNewsUseCase: ViewSearchedNewsUseCase): ViewModel() {
-    val query = MutableStateFlow("War")
+    private val viewSearchedNewsUseCase: ViewSearchedNewsUseCase,
+    private val viewTopHeadlinesUseCase: ViewTopHeadlinesUseCase
+): ViewModel() {
+    val _query = MutableStateFlow("War")
+    var query = _query.asStateFlow()
 
     fun searchForNews(query: String): Flow<PagingData<UiModel>> {
         return viewSearchedNewsUseCase.execute(query).map { pagingData -> pagingData.map { UiModel.NewsItem(it) } }
+    }
+    fun getTopHeadlines(): Flow<PagingData<UiModel>>{
+        return viewTopHeadlinesUseCase.execute().map { pagingData -> pagingData.map { UiModel.NewsItem(it) } }
     }
     fun saveArticle(uiModel: UiModel.NewsItem) = viewModelScope.launch{
         saveArticleUseCase.execute(uiModel.article)
