99e55cfb9a7bdfca49fd2b87147db8307dd16b5e
JackSaf <jacksafblaze@gmail.com>
2022-09-28 05:47:38 +0300

    added missing HiltViewModelAnnotation

--- app/src/main/java/com/jacksafblaze/newshub/presentation/viewmodel/NewsSearchViewModel.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/viewmodel/NewsSearchViewModel.kt
@@ -9,6 +9,7 @@ import com.jacksafblaze.newshub.domain.usecase.SaveArticleUseCase
 import com.jacksafblaze.newshub.domain.usecase.ViewSearchedNewsUseCase
 import com.jacksafblaze.newshub.domain.usecase.ViewTopHeadlinesUseCase
 import com.jacksafblaze.newshub.presentation.model.UiModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,12 +17,12 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class NewsSearchViewModel @Inject constructor(
     private val deleteSavedArticleUseCase: DeleteSavedArticleUseCase,
     private val saveArticleUseCase: SaveArticleUseCase,
     private val viewSearchedNewsUseCase: ViewSearchedNewsUseCase,
-    private val viewTopHeadlinesUseCase: ViewTopHeadlinesUseCase
-): ViewModel() {
+    private val viewTopHeadlinesUseCase: ViewTopHeadlinesUseCase): ViewModel() {
     val _query = MutableStateFlow("War")
     var query = _query.asStateFlow()
 
