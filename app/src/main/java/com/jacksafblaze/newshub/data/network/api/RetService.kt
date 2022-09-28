7b1c989f1ef911f78b4b7ab99ba6259025ce2414
JackSaf <jacksafblaze@gmail.com>
2022-09-28 09:52:47 +0300

    added sortBy parameter to getSearchedArticles method

--- app/src/main/java/com/jacksafblaze/newshub/data/network/api/RetService.kt
+++ app/src/main/java/com/jacksafblaze/newshub/data/network/api/RetService.kt
@@ -8,14 +8,19 @@ import retrofit2.http.Query
 
 interface RetService {
     @GET("v2/everything")
-    suspend fun getSearchedArticles(@Query("apiKey") apiKey: String = BuildConfig.API_KEY,
-                                    @Query("q") query: String,
-                                    @Query("pageSize")pageSize: Int,
-                                    @Query("page")page: Int): Response<NewsNetworkResult>
+    suspend fun getSearchedArticles(
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+        @Query("q") query: String,
+        @Query("pageSize") pageSize: Int,
+        @Query("page") page: Int,
+        @Query("sortBy") sortBy: String = "relevancy"
+    ): Response<NewsNetworkResult>
 
     @GET("v2/top-headlines")
-    suspend fun getTopHeadlines(@Query("apiKey") apiKey: String = BuildConfig.API_KEY,
-                                @Query("pageSize")pageSize: Int,
-                                @Query("page")page: Int,
-                                @Query("country")country: String = "us"): Response<NewsNetworkResult>
+    suspend fun getTopHeadlines(
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY,
+        @Query("pageSize") pageSize: Int,
+        @Query("page") page: Int,
+        @Query("country") country: String = "us"
+    ): Response<NewsNetworkResult>
 }
