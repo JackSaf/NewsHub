16e2bbb9a0ea9c371e70c49a5a92c26aaeab07c5
JackSaf <jacksafblaze@gmail.com>
2022-09-30 15:10:15 +0300

    implemented setOnClickListener inside of ViewHolder class

--- app/src/main/java/com/jacksafblaze/newshub/presentation/adapter/ArticleViewHolder.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/adapter/ArticleViewHolder.kt
@@ -5,13 +5,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.jacksafblaze.newshub.R
 import com.jacksafblaze.newshub.domain.model.Article
 import com.jacksafblaze.newshub.presentation.model.UiModel
 
-class ArticleViewHolder(private val view: View, private val onArticleSelected: (UiModel.NewsItem, ImageView) -> Unit): ViewHolder(view) {
+class ArticleViewHolder(private val view: View, private val onArticleSelected: (Article, ImageView) -> Unit): ViewHolder(view) {
+    private val rootItemLayout: CardView = view.findViewById(R.id.item_root_cardview)
     private val title: TextView = view.findViewById(R.id.article_title)
     private val description: TextView = view.findViewById(R.id.article_description)
     private val articleImage: ImageView = view.findViewById(R.id.article_image)
@@ -26,6 +28,9 @@ class ArticleViewHolder(private val view: View, private val onArticleSelected: (
         }
         if(article.urlToImage.isNotBlank()){
             Glide.with(view).load(article.urlToImage).into(articleImage)
+            rootItemLayout.setOnClickListener{
+                onArticleSelected(article, articleImage)
+            }
         }
         else{
             articleImage.visibility = View.GONE
@@ -33,7 +38,7 @@ class ArticleViewHolder(private val view: View, private val onArticleSelected: (
         timeAgo.text = TimeAgo.publishedAtToTimeAgo(article.publishedAt)
     }
     companion object{
-        fun create(parent: ViewGroup, onArticleSelected: (UiModel.NewsItem, ImageView) -> Unit): ArticleViewHolder{
+        fun create(parent: ViewGroup, onArticleSelected: (Article, ImageView) -> Unit): ArticleViewHolder{
             val view = LayoutInflater.from(parent.context).inflate(R.layout.news_view_item, parent, false)
             return ArticleViewHolder(view, onArticleSelected)
         }
