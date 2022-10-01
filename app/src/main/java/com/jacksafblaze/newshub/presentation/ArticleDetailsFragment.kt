35e0a2977536e114635abdc0100e873966a2ab25
JackSaf <jacksafblaze@gmail.com>
2022-10-01 08:09:23 +0300

    animated shared element transition

--- app/src/main/java/com/jacksafblaze/newshub/presentation/ArticleDetailsFragment.kt
+++ app/src/main/java/com/jacksafblaze/newshub/presentation/ArticleDetailsFragment.kt
@@ -1,14 +1,25 @@
 package com.jacksafblaze.newshub.presentation
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.MaterialContainerTransform
 import com.jacksafblaze.newshub.R
 
 
 class ArticleDetailsFragment : Fragment() {
+    val args: ArticleDetailsFragmentArgs by navArgs()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            duration = resources.getInteger(com.google.android.material.R.integer.material_motion_duration_long_1).toLong()
+            scrimColor = Color.TRANSPARENT
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
